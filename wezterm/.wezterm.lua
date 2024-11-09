655018eff3e9eb46597d37496533b51380d8947f
Maximilian Hagelstam <maximilian.hagelstam@smartum.fi>
2024-11-09 13:50:20 +0200

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,9 +2,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Aura (Gogh)'
+
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.window_decorations = "RESIZE"
 config.font_size = 14.0
+
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 
