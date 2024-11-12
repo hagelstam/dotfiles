631e3427bff86de95a8af5d6a059e743dd47ec45
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-11-12 17:33:24 +0200

    add windows specific wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,12 +1,15 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Windows only - runs WSL on startup
+-- config.default_domain = 'WSL:Ubuntu-24.04'
+
 config.color_scheme = 'Aura (Gogh)'
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 14.0
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE" -- TITLE on windows
 config.enable_tab_bar = false
 
 config.window_background_opacity = 0.8
